fa698ca8eb409a35d35ec2892b515c23e163db01
hasneenaljoker-netizen <hasneenaljoker@gmail.com>
2026-07-01 03:07:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,38 @@
 
+import os
+import yt_dlp
+from telegram import Update
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
+
+TOKEN = "8772485938:AAHjfw3UywpC-P6GiY7-BDDnM93kDdh--UU"
+
+def download_video(url):
+    ydl_opts = {
+        "outtmpl": "video.%(ext)s",
+        "format": "bestvideo+bestaudio/best",
+        "merge_output_format": "mp4",
+        "noplaylist": True,
+        "quiet": True,
+        "http_headers": {"User-Agent": "Mozilla/5.0"}
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        return ydl.prepare_filename(info)
+
+async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text.strip()
+
+    await update.message.reply_text("جاري التحميل...")
+
+    try:
+        file_path = download_video(url)
+        await update.message.reply_video(video=open(file_path, "rb"))
+        os.remove(file_path)
+    except Exception as e:
+        await update.message.reply_text(str(e))
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle))
+
+app.run_polling()
